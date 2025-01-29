9b3e1e464827d5731c20af0ce84f28699621cc5a
Shawn Hooper <shawn@fivesense.ca>
2025-01-29 13:30:19 -0500

    Update from upstream, v4

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # Twilio notifications channel for Laravel
 
-[![Latest Version on Packagist](https://img.shields.io/packagist/v/laravel-notification-channels/twilio.svg?style=flat-square)](https://packagist.org/packages/laravel-notification-channels/twilio)
-[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
-[![Build Status](https://img.shields.io/github/workflow/status/laravel-notification-channels/twilio/PHP?style=flat-square)](https://travis-ci.org/laravel-notification-channels/twilio)
-[![StyleCI](https://styleci.io/repos/65543339/shield)](https://styleci.io/repos/65543339)
-[![Quality Score](https://img.shields.io/scrutinizer/g/laravel-notification-channels/twilio.svg?style=flat-square)](https://scrutinizer-ci.com/g/laravel-notification-channels/twilio)
-[![Code Coverage](https://img.shields.io/scrutinizer/coverage/g/laravel-notification-channels/twilio/master.svg?style=flat-square)](https://scrutinizer-ci.com/g/laravel-notification-channels/twilio/?branch=master)
-[![Total Downloads](https://img.shields.io/packagist/dt/laravel-notification-channels/twilio.svg?style=flat-square)](https://packagist.org/packages/laravel-notification-channels/twilio)
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/laravel-notification-channels/twilio.svg)](https://packagist.org/packages/laravel-notification-channels/twilio)
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg)](LICENSE.md)
+[![Build Status](https://github.com/laravel-notification-channels/twilio/actions/workflows/ci.yml/badge.svg)](https://github.com/laravel-notification-channels/twilio/actions/workflows/ci.yml)
+[![Total Downloads](https://img.shields.io/packagist/dt/laravel-notification-channels/twilio.svg)](https://packagist.org/packages/laravel-notification-channels/twilio)
 
-This package makes it easy to send [Twilio notifications](https://documentation.twilio.com/docs) with Laravel 5.5+, 6.x and 7.x
-
-You are viewing the `3.x` documentation. [Click here](https://github.com/laravel-notification-channels/twilio/tree/2.x) to view the `2.x` documentation.
+This package makes it easy to send [Twilio notifications](https://documentation.twilio.com/docs) with Laravel 11.x
 
 ## Contents
 
@@ -26,10 +21,10 @@ You are viewing the `3.x` documentation. [Click here](https://github.com/laravel
 
 ## Installation
 
-You can install the package via composer:
+You can install the package via Composer:
 
 ``` bash
-composer require laravel-notification-channels/twilio
+$ composer require laravel-notification-channels/twilio
 ```
 
 ### Configuration
@@ -44,7 +39,8 @@ TWILIO_ACCOUNT_SID=1234 # always required
 TWILIO_FROM=100000000 # optional default from
 TWILIO_ALPHA_SENDER=HELLO # optional
 TWILIO_DEBUG_TO=23423423423 # Set a number that call calls/messages should be routed to for debugging
-TWILIO_SMS_SERVICE_SID=MG0a0aaaaaa00aa00a00a000a00000a00a # Optional but recommended 
+TWILIO_SMS_SERVICE_SID=MG0a0aaaaaa00aa00a00a000a00000a00a # Optional but recommended
+TWILIO_SHORTEN_URLS=true # optional, enable URL shortener
 ```
 
 ### Advanced configuration
@@ -57,7 +53,7 @@ Run `php artisan vendor:publish --provider="NotificationChannels\Twilio\TwilioPr
 #### Suppressing specific errors or all errors
 
 Publish the config using the above command, and edit the `ignored_error_codes` array. You can get the list of
-exception codes from [the documentation](https://www.twilio.com/docs/api/errors). 
+exception codes from [the documentation](https://www.twilio.com/docs/api/errors).
 
 If you want to suppress all errors, you can set the option to `['*']`. The errors will not be logged but notification
 failed events will still be emitted.
@@ -69,15 +65,19 @@ Twilio recommends always using a [Messaging Service](https://www.twilio.com/docs
 
 Having issues with SMS? Check Twilio's [best practices](https://www.twilio.com/docs/sms/services/services-best-practices).
 
+## Upgrading from 3.x to 4.x
+
+We have dropped support for PHP < 8.2 and the minimum Laravel version is now 11. Other than that, there are no breaking changes.
+
 ## Upgrading from 2.x to 3.x
 
-If you're upgrading from version `2.x`, you'll need to make sure that your set environment variables match those above 
-in the config section. None of the environment variable names have changed, but if you used different keys in your 
+If you're upgrading from version `2.x`, you'll need to make sure that your set environment variables match those above
+in the config section. None of the environment variable names have changed, but if you used different keys in your
 `services.php` config then you'll need to update them to match the above, or publish the config file and change the
 `env` key.
- 
+
 You should also remove the old entry for `twilio` from your `services.php` config, since it's no longer used.
- 
+
 The main breaking change between `2.x` and `3.x` is that failed notification will now throw an exception unless they are
 in the list of ignored error codes (publish the config file to edit these).
 
@@ -187,7 +187,7 @@ $ composer test
 
 ## Security
 
-If you discover any security related issues, please email gregoriohc@gmail.com instead of using the issue tracker.
+If you discover any security related issues, please email pipo@onlime.ch instead of using the issue tracker.
 
 ## Contributing
 
@@ -195,6 +195,8 @@ Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
 
 ## Credits
 
+- [Philip Iezzi (Pipo)](https://github.com/onlime)
+- [Pascal Baljet](https://github.com/pascalbaljet)
 - [Gregorio Hernández Caso](https://github.com/gregoriohc)
 - [atymic](https://github.com/atymic)
 - [All Contributors](../../contributors)
---
+++ tests/Unit/TwilioChannelTest.php
@@ -0,0 +1,294 @@
+<?php
+
+namespace NotificationChannels\Twilio\Tests\Unit;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Notifications\Events\NotificationFailed;
+use Illuminate\Notifications\Notification;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use NotificationChannels\Twilio\Exceptions\CouldNotSendNotification;
+use NotificationChannels\Twilio\Twilio;
+use NotificationChannels\Twilio\TwilioCallMessage;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioConfig;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+use Twilio\Exceptions\RestException;
+
+class TwilioChannelTest extends MockeryTestCase
+{
+    /** @var TwilioChannel */
+    protected $channel;
+
+    /** @var Twilio */
+    protected $twilio;
+
+    /** @var Dispatcher */
+    protected $dispatcher;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->twilio = Mockery::mock(Twilio::class);
+        $this->twilio->config = new TwilioConfig([]);
+        $this->dispatcher = Mockery::mock(Dispatcher::class);
+
+        $this->channel = new TwilioChannel($this->twilio, $this->dispatcher);
+    }
+
+    /** @test */
+    public function it_will_not_send_a_message_if_not_enabled()
+    {
+        $notifiable = new Notifiable;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'enabled' => false,
+        ]);
+
+        $this->dispatcher->shouldNotReceive('dispatch');
+
+        $result = $this->channel->send($notifiable, $notification);
+
+        $this->assertNull($result);
+    }
+
+    /** @test */
+    public function it_will_not_send_a_message_without_known_receiver()
+    {
+        $notifiable = new Notifiable;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'ignored_error_codes' => [],
+        ]);
+
+        $this->dispatcher->shouldReceive('dispatch')
+            ->atLeast()->once()
+            ->with(Mockery::type(NotificationFailed::class));
+
+        $this->expectException(CouldNotSendNotification::class);
+
+        $result = $this->channel->send($notifiable, $notification);
+
+        $this->assertNull($result);
+    }
+
+    /** @test */
+    public function it_will_send_a_sms_message_to_the_result_of_the_route_method_of_the_notifiable()
+    {
+        $notifiable = new NotifiableWithMethod;
+        $notification = Mockery::mock(Notification::class);
+
+        $message = new TwilioSmsMessage('Message text');
+        $notification->shouldReceive('toTwilio')->andReturn($message);
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->atLeast()->once()
+            ->with($message, '+1111111111', false);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_send_a_sms_message_to_the_result_of_the_route_method_of_the_notifiable_if_it_uses_the_twilio_channel_explicitly()
+    {
+        $notifiable = new NotifiableWithTwilioChannel;
+        $notification = Mockery::mock(Notification::class);
+
+        $message = new TwilioSmsMessage('Message text');
+        $notification->shouldReceive('toTwilio')->andReturn($message);
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->atLeast()->once()
+            ->with($message, '+1111111111', false);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_make_a_call_to_the_phone_number_attribute_of_the_notifiable()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $message = new TwilioCallMessage('http://example.com');
+        $notification->shouldReceive('toTwilio')->andReturn($message);
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->atLeast()->once()
+            ->with($message, '+22222222222', false);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_convert_a_string_to_a_sms_message()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $notification->shouldReceive('toTwilio')->andReturn('Message text');
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->atLeast()->once()
+            ->with(Mockery::type(TwilioSmsMessage::class), Mockery::any(), false);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_fire_an_event_in_case_of_an_invalid_message()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'ignored_error_codes' => [],
+        ]);
+
+        // Invalid message
+        $notification->shouldReceive('toTwilio')->andReturn(-1);
+
+        $this->dispatcher->shouldReceive('dispatch')
+            ->atLeast()->once()
+            ->with(Mockery::type(NotificationFailed::class));
+
+        $this->expectException(CouldNotSendNotification::class);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_ignore_specific_error_codes()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'ignored_error_codes' => [
+                44444,
+            ],
+        ]);
+
+        $notification->shouldReceive('toTwilio')->andReturn('Message text');
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->andThrow(new RestException('error', 44444, 400));
+
+        $this->dispatcher->shouldReceive('dispatch')
+            ->atLeast()->once()
+            ->with(Mockery::type(NotificationFailed::class));
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_rethrow_non_ignored_error_codes()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'ignored_error_codes' => [
+                55555,
+            ],
+        ]);
+
+        $notification->shouldReceive('toTwilio')->andReturn('Message text');
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->andThrow(new RestException('error', 44444, 400));
+
+        $this->dispatcher->shouldReceive('dispatch')
+            ->atLeast()->once()
+            ->with(Mockery::type(NotificationFailed::class));
+
+        $this->expectException(RestException::class);
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_ignore_all_error_codes()
+    {
+        $notifiable = new NotifiableWithAttribute;
+        $notification = Mockery::mock(Notification::class);
+
+        $this->twilio->config = new TwilioConfig([
+            'ignored_error_codes' => ['*'],
+        ]);
+
+        $notification->shouldReceive('toTwilio')->andReturn('Message text');
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->andThrow(new RestException('error', 44444, 400));
+
+        $this->dispatcher->shouldReceive('dispatch')
+            ->atLeast()->once()
+            ->with(Mockery::type(NotificationFailed::class));
+
+        $this->channel->send($notifiable, $notification);
+    }
+
+    /** @test */
+    public function it_will_send_using_alphanumeric_if_notifiable_can_receive()
+    {
+        $notifiable = new NotifiableWithAlphanumericSender;
+        $notification = Mockery::mock(Notification::class);
+
+        $message = new TwilioSmsMessage('Message text');
+        $notification->shouldReceive('toTwilio')->andReturn($message);
+
+        $this->twilio->shouldReceive('sendMessage')
+            ->atLeast()->once()
+            ->with($message, '+33333333333', true);
+
+        $this->channel->send($notifiable, $notification);
+    }
+}
+
+class Notifiable
+{
+    public $phone_number = null;
+
+    public function routeNotificationFor() {}
+}
+
+class NotifiableWithTwilioChannel
+{
+    public function routeNotificationFor(string $channel)
+    {
+        if ($channel === 'twilio') {
+            return '+1111111111';
+        }
+    }
+}
+
+class NotifiableWithMethod
+{
+    public function routeNotificationFor()
+    {
+        return '+1111111111';
+    }
+}
+
+class NotifiableWithAttribute
+{
+    public $phone_number = '+22222222222';
+
+    public function routeNotificationFor() {}
+}
+
+class NotifiableWithAlphanumericSender
+{
+    public $phone_number = '+33333333333';
+
+    public function routeNotificationFor() {}
+
+    public function canReceiveAlphanumericSender()
+    {
+        return true;
+    }
+}
